bcee6d4566720ef58a22135df2453e884dff5c4c
burn2705 <shevvg2004@yandex,ru>
2024-03-19 15:06:31 +0300

    migrations ver0

--- database/migrations/2024_03_19_093428_create_templates_table.php
+++ database/migrations/2024_03_19_093428_create_templates_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contracts', function (Blueprint $table) {
+        Schema::create('templates', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('client_id')->constrained();
-            $table->foreignId('template_id')->constrained();
-            $table->json('content');
+            $table->string('name');
+            $table->string('filename');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contracts');
+        Schema::dropIfExists('templates');
     }
 };
