4ba8c14aa8595c8ea7ca5e158dd4109a36be9c7e
burn2705 <shevvg2004@yandex,ru>
2024-03-19 13:33:04 +0300

    added models and some migrations

---
+++ app/Models/Ground.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/*
+    Таблица с основаниями для обращения
+
+*/
+
+
+class Ground extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+}
---
+++ database/migrations/2024_03_19_094924_create_clients_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_prefix'); // рассшифровка аббриветур
+            $table->string('company_name');
+            // представитель компании
+            $table->string('delegate_name');
+            $table->string('delegate_surname');
+            $table->string('delegate_th_name')->nullable();
+            $table->string('delegate_post');
+
+            $table->string('phone');
+            $table->string('bik');
+            $table->string('inn');
+            $table->string('kpp');
+            $table->string('address');
+            $table->string('payment_account');
+            $table->string('correspondent_account'); // корсчет
+            
+            $table->foreignId('bank_id')->constrained('banks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
