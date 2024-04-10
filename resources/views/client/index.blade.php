53210b43dfb386a4161f6326d9fe971c73dddbd3
burn2705 <shevvg2004@yandex,ru>
2024-04-10 10:50:48 +0300

    clients index page

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.master')
 {{-- {{ var_dump($clients) }} --}}
 
+@section('style')
+<style>
+
+</style>
+@endsection
+
 @section('content')
 
 <h1 class="pt-5">
     Все клиенты
 </h1>
 <div class="row gap-5">
-    <div class="col-7">
+    <div class="col-md" style="max-width: 1fr">
         <form action="#" class="pt-3 d-flex gap-3 mb-2">
             <div class="input-group rounded border-black border-1 border w-25">
                 <input type="search" class="form-control rounded border-0" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
@@ -41,9 +47,15 @@
                         <input type="checkbox" class="btn-check" id="btncheck{{$client['id']}}">
                         <label class="btn btn-outline-primary  pt-3" for="btncheck{{$client['id']}}"></label>
                     </td>
-                    <td class="pt-2">{{$client->name_prefix_short}} {{$client->company_name}}</td>
-                    <td class="pt-2">{{$client->delegate_surname}} {{$client->initials}}</td>
-                    <td class="pt-2">{{$client->phone}}</td>
+                    <td class="pt-2 table_crop_m" title="{{$client->name_prefix_short}} {{$client->company_name}}">
+                        {{$client->name_prefix_short}} {{$client->company_name}}
+                    </td>
+                    <td class="pt-2 table_crop_s" title="{{$client->delegate_surname}} {{$client->initials}}">
+                        {{$client->delegate_surname}} {{$client->initials}}
+                    </td>
+                    <td class="pt-2 table_crop_s" title="{{$client->phone}}">
+                        {{$client->phone}}
+                    </td>
                     <td class="pt-0 mt-0">
                         <x-table.two-buttons/>
                     </td>
@@ -55,6 +67,6 @@
             </tfoot>
         </table>
     </div>
-    {{-- <x-client-card/> --}}
+    <x-client-card/>
 </div>
 @endsection
